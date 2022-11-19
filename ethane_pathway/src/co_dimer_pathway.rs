7823b691dec3f31b25dfad02bd5dfca58e7bb7dc
TonyWu20 <tony.w21@gmail.com>
2022-11-20 00:08:08 +0800

    Add CO_dimer pathway adsorbates and yaml file

--- ethane_pathway/src/co_dimer_pathway.rs
+++ ethane_pathway/src/co_dimer_pathway.rs
@@ -7,15 +7,15 @@ use castep_model_generator_backend::{
     model_type::ModelInfo,
 };
 #[derive(Debug, Clone)]
-pub struct CH2Adsorbate<T: ModelInfo> {
+pub struct COAdsorbate<T: ModelInfo> {
     lattice_model: LatticeModel<T>,
     ads_info: AdsInfo,
 }
 
 /// Label it with the `Adsorbate` trait
-impl<T: ModelInfo> Adsorbate for CH2Adsorbate<T> {}
+impl<T: ModelInfo> Adsorbate for COAdsorbate<T> {}
 
-impl<T: ModelInfo> CH2Adsorbate<T> {
+impl<T: ModelInfo> COAdsorbate<T> {
     pub fn new(lattice_model: LatticeModel<T>, ads_info: AdsInfo) -> Self {
         Self {
             lattice_model,
@@ -23,14 +23,14 @@ impl<T: ModelInfo> CH2Adsorbate<T> {
         }
     }
 }
-
-impl From<&AdsInfo> for CH2Adsorbate<MsiModel> {
+impl From<&AdsInfo> for COAdsorbate<MsiModel> {
     fn from(item: &AdsInfo) -> Self {
         let ads_name = item.name().to_owned();
         let cwd = env!("CARGO_MANIFEST_DIR");
-        let filepath = format!("{}/adsorbates/CH2_coupling/{ads_name}.msi", cwd);
+        let filepath = format!("{}/adsorbates/CO_dimer/{ads_name}.msi", cwd);
         let msi_content = fs::read_to_string(filepath).unwrap();
-        let msi_model = LatticeModel::try_from(msi_content.as_str()).unwrap();
+        let msi_model = LatticeModel::try_from(msi_content.as_str())
+            .unwrap_or_else(|e| panic!("{}: Error while parsing {}", e, ads_name));
         Self::new(msi_model, item.to_owned())
     }
 }
--- ethane_pathway/src/lib.rs
+++ ethane_pathway/src/lib.rs
@@ -1,14 +1,14 @@
 #![allow(dead_code, non_snake_case)]
-extern crate nalgebra as na;
 
 pub mod ch2_pathway;
+pub mod co_dimer_pathway;
 
 #[cfg(test)]
 #[test]
 fn parse_adsorbate() {
     use castep_model_generator_backend::external_info::{adsorbate_table::AdsTab, YamlTable};
 
-    use crate::ch2_pathway::CH2Adsorbate;
+    use crate::{ch2_pathway::CH2Adsorbate, co_dimer_pathway::COAdsorbate};
 
     let cwd = env!("CARGO_MANIFEST_DIR");
     println!("{}", cwd);
@@ -17,4 +17,13 @@ fn parse_adsorbate() {
         let ads = CH2Adsorbate::from(ads_info);
         println!("{:#?}", ads);
     });
+    let co_ads_table = AdsTab::load_table("ethane_co_dimer.yaml").unwrap();
+    co_ads_table
+        .adsorbates()
+        .unwrap()
+        .iter()
+        .for_each(|ads_info| {
+            let ads = COAdsorbate::from(ads_info);
+            println!("{:#?}", ads);
+        });
 }
