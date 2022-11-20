b4dc1f6edde78d71a378774b1a12a137c1410079
TonyWu20 <tony.w21@gmail.com>
2022-11-21 01:48:43 +0800

    Hardcoded coordsite info

--- basic_models/src/gdy_lattice/mod.rs
+++ basic_models/src/gdy_lattice/mod.rs
@@ -6,20 +6,59 @@ pub struct GDYLattice<T: ModelInfo> {
     lattice: LatticeModel<T>,
     lattice_name: String,
     metal_site: u32,
-    coord_sites: [CoordSite; 7],
 }
 
 #[derive(Debug, Clone)]
-pub struct CoordSite {
-    site_name: String,
+pub struct CoordSite<'a> {
+    site_name: &'a str,
     site_id: u32,
 }
 
-impl CoordSite {
+pub const GDY_SINGLE_CASES: [u32; 7] = [41, 42, 54, 53, 52, 40, 73];
+pub const GDY_DOUBLE_CASES: [[u32; 2]; 7] = [
+    [41, 42],
+    [42, 54],
+    [54, 53],
+    [53, 52],
+    [41, 40],
+    [41, 73],
+    [42, 73],
+];
+pub const GDY_COORD_SITES: [CoordSite; 7] = [
+    CoordSite {
+        site_name: "c1",
+        site_id: 41,
+    },
+    CoordSite {
+        site_name: "c2",
+        site_id: 42,
+    },
+    CoordSite {
+        site_name: "c3",
+        site_id: 54,
+    },
+    CoordSite {
+        site_name: "c4",
+        site_id: 53,
+    },
+    CoordSite {
+        site_name: "FR",
+        site_id: 52,
+    },
+    CoordSite {
+        site_name: "NR",
+        site_id: 40,
+    },
+    CoordSite {
+        site_name: "M",
+        site_id: 73,
+    },
+];
+
+impl<'a> CoordSite<'a> {
     pub fn site_name(&self) -> &str {
         self.site_name.as_ref()
     }
-
     pub fn site_id(&self) -> u32 {
         self.site_id
     }
@@ -27,41 +66,10 @@ impl CoordSite {
 
 impl<T: ModelInfo> GDYLattice<T> {
     pub fn new(lattice: LatticeModel<T>, lattice_name: String) -> Self {
-        let coord_sites: [CoordSite; 7] = [
-            CoordSite {
-                site_name: "c1".into(),
-                site_id: 41,
-            },
-            CoordSite {
-                site_name: "c2".into(),
-                site_id: 42,
-            },
-            CoordSite {
-                site_name: "c3".into(),
-                site_id: 54,
-            },
-            CoordSite {
-                site_name: "c4".into(),
-                site_id: 53,
-            },
-            CoordSite {
-                site_name: "FR".into(),
-                site_id: 52,
-            },
-            CoordSite {
-                site_name: "NR".into(),
-                site_id: 40,
-            },
-            CoordSite {
-                site_name: "M".into(),
-                site_id: 73,
-            },
-        ];
         Self {
             lattice,
             lattice_name,
             metal_site: 73,
-            coord_sites,
         }
     }
 
@@ -83,10 +91,6 @@ impl<T: ModelInfo> GDYLattice<T> {
 
     pub fn lattice_mut(&mut self) -> &mut LatticeModel<T> {
         &mut self.lattice
-    }
-
-    pub fn coord_sites(&self) -> &[CoordSite; 7] {
-        &self.coord_sites
     }
 }
 
