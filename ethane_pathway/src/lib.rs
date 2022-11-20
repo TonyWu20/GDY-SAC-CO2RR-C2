764f218b2a22fa7bedd087faa0f4930b10b72d82
TonyWu20 <tony.w21@gmail.com>
2022-11-20 23:27:20 +0800

    Reduce redundant `From` trait implementations using generic type id matching

--- ethane_pathway/src/lib.rs
+++ ethane_pathway/src/lib.rs
@@ -1,17 +1,24 @@
 #![allow(dead_code, non_snake_case)]
 
-use std::marker::PhantomData;
+use std::{any::TypeId, fs, marker::PhantomData};
 
 use castep_model_generator_backend::{
     adsorbate::{AdsInfo, Adsorbate},
     lattice::LatticeModel,
-    model_type::ModelInfo,
+    model_type::{msi::MsiModel, ModelInfo},
 };
 
-pub mod ch2_pathway;
-pub mod co_dimer_pathway;
-
 pub trait Pathway {}
+
+#[derive(Debug)]
+/// Marker struct to represent "CH2 coupling pathway"
+pub struct CH2Pathway;
+impl Pathway for CH2Pathway {}
+
+#[derive(Debug)]
+/// Marker struct to represent "CO dimerization pathway"
+pub struct COPathway;
+impl Pathway for COPathway {}
 
 #[derive(Debug, Clone)]
 /// Generic struct for Adsorption models from different pathway and in different format.
@@ -31,6 +38,14 @@ impl<'a, T: ModelInfo, P: Pathway> AdsModel<'a, T, P> {
             pathway: PhantomData,
         }
     }
+
+    pub fn lattice_model(&self) -> &LatticeModel<T> {
+        &self.lattice_model
+    }
+
+    pub fn ads_info(&self) -> &AdsInfo {
+        self.ads_info
+    }
 }
 
 impl<'a, T, P> Adsorbate for AdsModel<'a, T, P>
@@ -38,6 +53,27 @@ where
     T: ModelInfo,
     P: Pathway,
 {
+}
+impl<'a, P> From<&'a AdsInfo> for AdsModel<'a, MsiModel, P>
+where
+    P: Pathway + 'static,
+{
+    fn from(item: &'a AdsInfo) -> Self {
+        let ads_name = item.name();
+        let cwd = env!("CARGO_MANIFEST_DIR");
+        let path_type_id = TypeId::of::<P>();
+        let pathway_dir = if path_type_id == TypeId::of::<CH2Pathway>() {
+            "CH2_coupling"
+        } else if path_type_id == TypeId::of::<COPathway>() {
+            "CO_dimer"
+        } else {
+            panic!("Wrong pathway type!")
+        };
+        let filepath = format!("{}/adsorbates/{}/{}.msi", cwd, pathway_dir, ads_name);
+        let msi_content = fs::read_to_string(filepath).unwrap();
+        let msi_model = LatticeModel::try_from(msi_content.as_str()).unwrap();
+        Self::new(msi_model, item)
+    }
 }
 
 #[cfg(test)]
@@ -47,8 +83,6 @@ fn parse_adsorbate() {
         external_info::{adsorbate_table::AdsTab, YamlTable},
         model_type::msi::MsiModel,
     };
-
-    use crate::{ch2_pathway::CH2Pathway, co_dimer_pathway::COPathway};
 
     let cwd = env!("CARGO_MANIFEST_DIR");
     println!("{}", cwd);
