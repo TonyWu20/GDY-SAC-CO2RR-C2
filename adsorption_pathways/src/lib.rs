40513eee589ad70f986aaac841ed35561a92b4e8
TonyWu20 <tony.w21@gmail.com>
2022-12-08 22:24:35 +0800

    Add ethyne

--- adsorption_pathways/src/lib.rs
+++ adsorption_pathways/src/lib.rs
@@ -5,9 +5,11 @@ use std::{any::TypeId, fs, marker::PhantomData};
 use castep_model_core::{LatticeModel, ModelInfo, MsiModel};
 use castep_model_generator_backend::adsorbate::{AdsInfo, Adsorbate};
 pub mod ethane_pathway;
+pub mod ethyne_pathway;
 pub mod water_pathway;
 
 pub use ethane_pathway::{CH2Pathway, COPathway};
+pub use ethyne_pathway::EthynePathway;
 pub use water_pathway::Water;
 
 #[cfg(test)]
@@ -22,17 +24,20 @@ pub enum PathwayId {
     CH2,
     CO,
     Water,
+    Ethyne,
     Undefined,
 }
 
 impl PathwayId {
-    fn new(type_id: TypeId) -> Self {
+    pub fn new(type_id: TypeId) -> Self {
         if type_id == TypeId::of::<CH2Pathway>() {
             Self::CH2
         } else if type_id == TypeId::of::<COPathway>() {
             Self::CO
         } else if type_id == TypeId::of::<Water>() {
             Self::Water
+        } else if type_id == TypeId::of::<EthynePathway>() {
+            Self::Ethyne
         } else {
             Self::Undefined
         }
@@ -42,6 +47,16 @@ impl PathwayId {
             Self::CH2 => Some("CH2_coupling"),
             Self::CO => Some("CO_dimer"),
             Self::Water => Some("Water"),
+            Self::Ethyne => Some("ethyne"),
+            Self::Undefined => None,
+        }
+    }
+    pub fn target_dir(&self) -> Option<&str> {
+        match *self {
+            Self::CH2 => Some("CH2_coupling"),
+            Self::CO => Some("CO_dimer"),
+            Self::Water => Some("Water"),
+            Self::Ethyne => Some("ethyne"),
             Self::Undefined => None,
         }
     }
@@ -51,13 +66,13 @@ impl PathwayId {
 /// Generic struct for Adsorption models from different pathway and in different format.
 /// The struct has a lifetime as long as the `&AdsInfo`. The `&AdsInfo` is borrowed
 /// from the `AdsTab` which we will load at the beginning of the workflow.
-pub struct AdsModel<'a, T: ModelInfo, P: Pathway> {
+pub struct AdsModel<'a, T: ModelInfo, P: Pathway, const N: usize> {
     lattice_model: LatticeModel<T>,
     ads_info: &'a AdsInfo,
     pathway: PhantomData<P>,
 }
 
-impl<'a, T: ModelInfo, P: Pathway> AdsModel<'a, T, P> {
+impl<'a, T: ModelInfo, P: Pathway, const N: usize> AdsModel<'a, T, P, N> {
     pub fn new(lattice_model: LatticeModel<T>, ads_info: &'a AdsInfo) -> Self {
         Self {
             lattice_model,
@@ -75,13 +90,13 @@ impl<'a, T: ModelInfo, P: Pathway> AdsModel<'a, T, P> {
     }
 }
 
-impl<'a, T, P> Adsorbate for AdsModel<'a, T, P>
+impl<'a, T, P, const N: usize> Adsorbate for AdsModel<'a, T, P, N>
 where
     T: ModelInfo,
     P: Pathway,
 {
 }
-impl<'a, P> From<&'a AdsInfo> for AdsModel<'a, MsiModel, P>
+impl<'a, P, const N: usize> From<&'a AdsInfo> for AdsModel<'a, MsiModel, P, N>
 where
     P: Pathway + 'static,
 {
